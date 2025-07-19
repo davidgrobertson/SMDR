f916bfa6ccc06706a818549e839e34ad1cc49c18
David Robertson <drobertson@otterbein.edu>
2025-07-18 23:49:56 -0400

    Delete fig_data/smdr_build.h

--- fig_data/smdr_build.h
+++
@@ -1,9 +0,0 @@
-/* Program name and version information */
-
-#ifndef _SMDR_VERSION_H_
-#define _SMDR_VERSION_H_
-
-#define SMDR_NAME       "SMDR (Standard Model in Dimensional Regularization)"
-#define SMDR_VERSION    "1.3"
-
-#endif
